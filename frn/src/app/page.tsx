bd2a38cf6f352ae6300e5f6f74ceaf2d62693163
AsadbekNurmamatov2002 <nurmamatovasadbek85@gmail.com>
2024-05-05 22:29:07 +0500

    m21

---
+++ frn/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Responsive from "@/components/Responsive";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Responsive />
+    </main>
+  );
+}
---
+++ frn/src/components/Responsive.tsx
@@ -0,0 +1,12 @@
+
+const Responsive = () => {
+  return (
+    <div className='sm:bg-black sm:text-white md:bg-green-600 md:text-yellow-50 lg:bg-res-500 lg:text-[#52223] xl:bg-blue-500 xl:text-white 2xl:bg-gray-900 2xl:text-white'>
+      <div className='w-[100px] h-[100px] shadow-sm shadow-orange-100 rounded-lg flex justify-center items-center'>
+        Hello Responsive 
+      </div>
+    </div>
+  )
+}
+
+export default Responsive
